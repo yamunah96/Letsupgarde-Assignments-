14ea799549349aa14759d72186da56dca1014df4
yamunah96 <70136290+yamunah96@users.noreply.github.com>
2021-03-26 19:46:49 +0530

    Add files via upload

---
+++ Day4-Assignmet/first.js
@@ -0,0 +1,12 @@
+var tag=document.getElementsByTagName("div");
+//console.log(tag);
+var p=document.getElementsByTagName("p");
+console.log(p)
+//console.log(p[0].innerHTML="<h1>change the paragraph content</h1>")
+
+
+var clas= document.getElementsByClassName("item");
+//console.log(clas);
+
+var id= document.getElementById("box");
+//console.log(id.placeholder)
